890885c3fefee68de9a4b31f9d3e594d7ed04075
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2024-12-08 02:20:19 +0530

    Update InfiniteCards.tsx

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -15,6 +15,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
+    link: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
